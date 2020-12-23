4d5be58cee590fbaaf7241eec399e6585467445f
install <kiosun@yandex.ru>
2020-12-24 00:49:26 +0300

    next 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            System.out.println(arg);
+
+        }
     }
 }
