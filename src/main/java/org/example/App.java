8d01ba0b8b8d52759bdc37d8ec5564c9bfade0c0
install <kiosun@yandex.ru>
2020-12-25 09:35:16 +0300

    next 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,37 @@
 package org.example;
 
+import java.io.PrintStream;
+
 /**
  * Hello world!
  *
  */
 public class App
 {
-    public static void main( String[] args )
+    public App() {
+        super();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public static void main(String[] args )
     {
-        System.out.println( "Hello World!" );
+        final PrintStream out = getOut();
+        out.println( "Hello World!" );
         for (int i = 0; i < args.length; i++) {
             String arg = args[i];
-            System.out.println(arg);
-
+            out.println(arg);
         }
+        for (String arg : args) {
+            out.println(arg);
+        }
+
+    }
+
+    private static PrintStream getOut() {
+        return System.out;
     }
 }
